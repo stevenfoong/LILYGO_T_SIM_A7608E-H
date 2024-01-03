1a555e36422ed2364536328eaec1cf5594d077eb
Steven Foong <foongws@ecsmy.com>
2024-01-03 15:53:32 +0800

    Delete SendSMS/config.h

--- SendSMS/config.h
+++
@@ -1,9 +0,0 @@
-const char* default_ssid = "wifi-ssid";
-const char* default_password = "wifi-password";
-const char* ap_ssid = "tsim";
-const char* ap_password = "88888888";
-
-String default_WebHookURL = "https://Default-webhock";
-
-String SMS_Number = "0123456789";
-
